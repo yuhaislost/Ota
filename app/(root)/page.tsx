30e4a69287967b300100bb222f56a71115be7464
Yuhang Li <68719011+yuhaislost@users.noreply.github.com>
2023-08-22 00:13:04 +1000

    Creates basic Thread Card UI, creates thread actions with fetch threads and comments immplementing impagination, fixes hydration error caused by loading thread cards

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,62 @@
 //app/page.tsx
+import { fetchThreads } from "@/lib/actions/thread.actions";
 import { UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs";
+import { fetchThreadComments } from "@/lib/actions/thread.actions";
+import ThreadCard from "@/components/cards/ThreadCard";
 
-export default function Home() {
+export default async function Home() {
+  const user = await currentUser();
+  const threadsResult = await fetchThreads(1, 20);
+  const commentsMap = new Map<string, any>();
+  for (let i = 0; i < threadsResult.fetchedThreads.length; i++)
+  {
+    const commentData = await fetchThreadComments(threadsResult.fetchedThreads[i]._id, 1, 20);
+    let comments: any = commentData.comments;
+    if (comments.length === 0)
+    {
+      commentsMap.set(threadsResult.fetchedThreads[i]._id, null);
+    }
+    else{
+      commentsMap.set(threadsResult.fetchedThreads[i]._id, comments);
+    }
+  };
+
+
+  console.log(threadsResult.fetchedThreads);
+
+
+  //BIG NONONO CAUSES HYDRATION ERROR WHEN CALLED TO GENERATE CARDS
+  // async function cardGenerator(thread: any)
+  // {
+  //   if (comments.length === 0)
+  //   {
+  //     comments = null; 
+  //   }
+
+  //   return(
+  //     <ThreadCard 
+  //       key={thread.id} id={thread.id} currentUserId={user?.id || ""} parentId={thread.parentId} content={thread.text} author={thread.author} 
+  //       community={thread.community} createdAt={thread.createdAt} comments={comments}
+  //     />
+  //   );
+  // }
+
   return (
     <>
       <h1 className="head-text text-left">Home</h1>
-      <UserButton afterSignOutUrl="/"/>
+      <section className="mt-9 flex flex-col gap-10">
+        {
+          threadsResult.fetchedThreads.length === 0 ? (
+            <p className="no-result">No Threads found</p>
+          ): (
+              threadsResult.fetchedThreads.map((thread) =>  (
+              <ThreadCard 
+                key={thread.id} id={thread.id} currentUserId={user?.id || ""} parentId={thread.parentId} content={thread.text} author={thread.author} 
+                community={thread.community} createdAt={thread.createdAt} comments={commentsMap.get(thread._id)}
+              />)))
+        }
+      </section>
     </>
-  )
+  );
 }
--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -3,6 +3,8 @@
 import { revalidatePath } from "next/cache";
 import Thread from "../models/ThreadModel";
 import { connectToDb } from "../mongoose";
+import User from "../models/UserModel";
+import { ObjectId } from "mongodb";
 
 export async function createThread(text: string, author: string, communityId:  string | null, path: string){
     try{
@@ -18,5 +20,48 @@ export async function createThread(text: string, author: string, communityId:  s
     }catch(error: any)
     {
         throw new Error(`Internal Server Error. Error: ${error.message}`);
+    }
+}
+
+export async function fetchThreads(page = 1, pageSize = 20)
+{
+    connectToDb();
+
+    const skip = (page - 1) * pageSize;
+
+    try{ 
+        const threadQuery = Thread.find({ parentId: {$in: [null, undefined]}}).sort({createdAt: 'desc'}).skip(skip).limit(pageSize).populate({path: 'author', model: User});
+
+        const totalThreadCount = await Thread.countDocuments({parentId: {$in: [null, undefined]}});
+
+        const fetchedThreads = await threadQuery.exec();
+
+        const nextPage = totalThreadCount > skip + fetchedThreads.length;
+
+        return {fetchedThreads, nextPage};
+
+    }catch(error: any)
+    {
+        throw new Error(`Internal Server Error. Error: ${error.message}`);
+    }
+}
+
+export async function fetchThreadComments(threadId: ObjectId, page = 1, pageSize = 20)
+{
+    connectToDb();
+    const skip = (page - 1) * pageSize;
+
+    try{
+        const commentsQuery = Thread.find({parentId: threadId}).sort({createdAt: 'desc'}).skip(skip).limit(pageSize).populate({path: 'author', model: User});
+        const comments = await commentsQuery.exec();
+        const totalCommentCount = await Thread.countDocuments({parentId: threadId});
+
+        const nextPage = totalCommentCount > skip + comments.length;
+
+        return {comments, nextPage};
+
+    }catch(error: any)
+    {
+        throw new Error(`Internal Server Error: ${error.message}`);
     }
 }
