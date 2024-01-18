7943b89af4c0cd8420e8a694a3de3e40e9505230
Yuhang Li <68719011+yuhaislost@users.noreply.github.com>
2024-01-18 14:29:35 +1100

    Revert "Added Search Page"
    This reverts commit 1e60f9fff1e02f10626e3c857213a297f946bd7a.

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -19,6 +19,20 @@ async function Page()
     return(
         <section>
             <h1 className="head-text mb-10">Search</h1>
+
+            <div className="mt-14 flex flex-col gap-9">
+                {
+                    fetchedUsers.length === 0 ? (
+                        <p className="no-result">No Users</p>
+                    ) : (
+                        <>
+                            {fetchedUsers.map((fetchedUser) => (<UserCard key={fetchedUser.id} id={fetchedUser.id} name={fetchedUser.name} username={fetchedUser.username}
+                                profileImage={fetchedUser.profileImage} userType="User"
+                            />))}
+                        </>
+                    )
+                }
+            </div>
         </section>
     )
 }
